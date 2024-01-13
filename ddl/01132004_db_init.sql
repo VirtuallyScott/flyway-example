bc33f384a34bd2926a0ca4ba2a70f7b4667b6ce0
Scott S. <scott.smith@virtuallyscott.com>
2024-01-13 13:47:13 -0500

    add init ddl

---
+++ ddl/01132004_db_init.sql
@@ -0,0 +1,20 @@
+-- Create the names table
+CREATE TABLE names (
+    id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-4' || lower(substr(hex(randomblob(2)),2)) || '-' || substr('89AB',abs(random()) % 4 + 1, 1) || lower(substr(hex(randomblob(2)),2)) || '-' || lower(hex(randomblob(6)))),
+    start_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    f_name TEXT NOT NULL,
+    m_name TEXT,
+    l_name TEXT NOT NULL
+);
+
+-- Trigger to set start_date
+CREATE TRIGGER set_start_date BEFORE INSERT ON names
+FOR EACH ROW
+BEGIN
+    SELECT CASE
+        WHEN NEW.start_date IS NULL THEN
+            SET NEW.start_date = CURRENT_TIMESTAMP;
+        ELSE
+            NEW.start_date;
+    END;
+END;
