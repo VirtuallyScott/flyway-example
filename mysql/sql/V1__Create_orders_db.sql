4452b49d42bd88f6da83c2cf738395a0b25feb7c
Scott S. <scott.smith@virtuallyscott.com>
2024-01-18 08:58:46 -0500

    update to add mysql stuff

---
+++ mysql/sql/V1__Create_orders_db.sql
@@ -0,0 +1,12 @@
+-- Filename: V1__Create_orders_db.sql
+CREATE DATABASE IF NOT EXISTS orders;
+USE orders;
+
+-- You can also include further table creation or other DDL statements here, for example:
+-- CREATE TABLE order_details (
+--     id INT AUTO_INCREMENT,
+--     order_id INT,
+--     product_name VARCHAR(255),
+--     quantity INT,
+--     PRIMARY KEY (id)
+-- );
